0a84f0d6b8b6866f9cdd5937254faa262d56ce7e
betoab728 <betoab728@gmail.com>
2025-06-09 00:52:28 -0500

    configuracion cors

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ from fastapi import FastAPI
 from app.api.routes import rol_routes
 from app.api.routes.usuario_routes import router as usuario_router
 from app.api.routes.auth_routes import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Sistema de Gestión de Residuos")
 app.include_router(auth_router) 
 app.include_router(rol_routes.router)
 app.include_router(usuario_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
