e4b39c941537784c8c11fc6c69983f75ef11ccad
betoab728 <betoab728@gmail.com>
2025-08-27 21:55:56 -0500

    allow origin

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app.include_router(generador_residuo_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
