2353c4655d76867f82f30cf6afaa7166a0c32cde
betoab728 <betoab728@gmail.com>
2025-08-28 08:29:33 -0500

    cors rrrr

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app.include_router(generador_residuo_router)
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
