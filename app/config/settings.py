44ec5f53826ddd3ffeadb651340099546d3e68bf
betoab728 <betoab728@gmail.com>
2025-11-03 16:43:51 -0500

    ajuste variable port

--- app/config/settings.py
+++ app/config/settings.py
@@ -10,10 +10,10 @@ class Settings(BaseSettings):
     app_version: str = os.getenv("APP_VERSION")
 
     postgres_host: str = os.getenv("POSTGRES_HOST")
-    postgres_port: int = int(os.getenv("POSTGRES_PORT"))
     postgres_user: str = os.getenv("POSTGRES_USER")
     postgres_password: str = os.getenv("POSTGRES_PASSWORD")
     postgres_db: str = os.getenv("POSTGRES_DB")
+     postgres_port: int = int(os.getenv("POSTGRES_PORT"))
     secret_key: str = os.getenv("SECRET_KEY")
     #aws s3
     aws_access_key_id: str = os.getenv("AWS_ACCESS_KEY_ID")
