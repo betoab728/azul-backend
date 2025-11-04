bc82587dc603564c895824e19bb24c4132cc8b9b
betoab728 <betoab728@gmail.com>
2025-11-04 09:16:51 -0500

    actualizar seting

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,26 +1,38 @@
 from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 class Settings(BaseSettings):
-    app_name: str = os.getenv("APP_NAME")
-    app_version: str = os.getenv("APP_VERSION")
+    app_name: str | None = None
+    app_version: str | None = None
 
-    postgres_host: str = os.getenv("POSTGRES_HOST")
-    postgres_port: str = os.getenv("POSTGRES_PORT", "5432")  # <- cambiamos a str
-    postgres_user: str = os.getenv("POSTGRES_USER")
-    postgres_password: str = os.getenv("POSTGRES_PASSWORD")
-    postgres_db: str = os.getenv("POSTGRES_DB")
+    postgres_host: str | None = None
+    postgres_port: str | None = "5432"
+    postgres_user: str | None = None
+    postgres_password: str | None = None
+    postgres_db: str | None = None
+    secret_key: str | None = None
 
-    environment: str = os.getenv("ENVIRONMENT")
+    # AWS S3 (opcionales)
+    aws_access_key_id: str | None = None
+    aws_secret_access_key: str | None = None
+    aws_region: str | None = None
+    aws_s3_bucket_name: str | None = None
+
+    environment: str | None = None
 
     @property
     def database_url(self) -> str:
-        return (
-            f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
-            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
-        )
+        if all([self.postgres_user, self.postgres_password, self.postgres_host, self.postgres_db]):
+            return (
+                f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
+                f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+            )
+        else:
+            # Valor por defecto o advertencia si falta algo
+            print("⚠️ Advertencia: faltan variables de conexión a la base de datos")
+            return ""
+
+    class Config:
+        env_file = ".env"  # para entorno local
+        env_file_encoding = "utf-8"
 
 settings = Settings()
