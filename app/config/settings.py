e62131760f2f7fa57931562e216d459806b6fdc8
betoab728 <betoab728@gmail.com>
2025-11-04 11:07:27 -0500

    variables entorno mayusc

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,32 +1,32 @@
 from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
-import os
+#from dotenv import load_dotenv
+#import os
 
 # Cargar variables desde .env
-load_dotenv()
+#load_dotenv()
 
 class Settings(BaseSettings):
     # Ya NO necesitas asignar os.getenv(...)
     # Pydantic-settings buscará automáticamente estas variables en el entorno
-    app_name: str
-    app_version: str
+    APP_NAME: str
+    APP_VERSION: str
 
-    postgres_host: str
+    POSTGRES_HOST: str
     # Lo he dejado como str para que coincida con la validación,
     # aunque lo ideal sería usar int si es un puerto
-    postgres_port: str 
-    postgres_user: str
-    postgres_password: str
-    postgres_db: str
+    POSTGRES_PORT: str 
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
 
-    environment: str
+    ENVIRONMENT: str
 
     @property
     def database_url(self) -> str:
         # Asegúrate de usar f-strings multilinea correctamente si mantienes este formato
         return (
-            f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
-            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+            f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
         )
 
 # Cuando instancies Settings(), pydantic-settings llenará automáticamente los campos
