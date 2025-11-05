37715ad5f1a26afb6cc83aef69a8a3bf8ef239af
betoab728 <betoab728@gmail.com>
2025-11-04 19:16:35 -0500

    merge2

--- app/infrastructure/db/database.py
+++ app/infrastructure/db/database.py
@@ -3,13 +3,10 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
-<<<<<<< HEAD
 from app.config.settings import settings
-=======
 from sqlmodel import SQLModel
-
 #DATABASE_URL = settings.database_url
->>>>>>> 27a7994ec0d0ba6a943a54bfeac5d03f2eaf10c6
+
 
 engine = create_async_engine(
     DATABASE_URL,
