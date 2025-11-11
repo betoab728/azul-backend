0297aed6440a796784cecaa3faa077e81eb180b9
betoab728 <betoab728@gmail.com>
2025-11-10 21:50:03 -0500

    hora

--- app/api/dtos/solicitud_cotizacion_dto.py
+++ app/api/dtos/solicitud_cotizacion_dto.py
@@ -86,6 +86,7 @@ class SolicitudConDatosReadDto(BaseModel):
     estado_solicitud: str
     embarcacion: str
     generador: str
+    created_at: datetime
 
     @field_serializer("fecha")
     def serialize_fecha(self, value: date) -> str:
