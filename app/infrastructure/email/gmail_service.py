67861e82697f5fde415b9eac335900875083453d
betoab728 <betoab728@gmail.com>
2025-12-31 12:57:48 -0500

    envio correo

--- app/infrastructure/email/gmail_service.py
+++ app/infrastructure/email/gmail_service.py
@@ -2,15 +2,12 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from app.config.settings import settings
+from fastapi.concurrency import run_in_threadpool
 
 class GmailEmailService:
 
     async def enviar_email(self, to_email: str, subject: str, html_content: str):
 
-        if not settings.SMTP_USER or not settings.SMTP_PASSWORD:
-            print("credenciales SMTP no configurado")
-            return
-        
         if not all([
             settings.SMTP_HOST,
             settings.SMTP_PORT,
@@ -20,16 +17,15 @@ class GmailEmailService:
             print("SMTP incompleto, no se envía email")
             return
 
+        def _send():
+            msg = MIMEMultipart("alternative")
+            msg["From"] = f"{settings.MAIL_FROM_NAME} <{settings.MAIL_FROM}>"
+            msg["To"] = to_email
+            msg["Subject"] = subject
 
-        msg = MIMEMultipart("alternative")
-        msg["From"] = f"{settings.MAIL_FROM_NAME} <{settings.MAIL_FROM}>"
-        msg["To"] = to_email
-        msg["Subject"] = subject
+            msg.attach(MIMEText(html_content, "html"))
 
-        msg.attach(MIMEText(html_content, "html"))
-
-        try:
-            with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            with smtplib.SMTP(settings.SMTP_HOST, int(settings.SMTP_PORT)) as server:
                 server.starttls()
                 server.login(settings.SMTP_USER, settings.SMTP_PASSWORD)
                 server.sendmail(
@@ -37,5 +33,8 @@ class GmailEmailService:
                     to_email,
                     msg.as_string()
                 )
+
+        try:
+            await run_in_threadpool(_send)
         except Exception as e:
             print("Error enviando email:", e)
